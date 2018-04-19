b715eb256c8e0957087024c0aaba2ce62a7980af
Valehelle <hazmiirfan92@gmail.com>
2018-04-19 14:31:12 +0800

    create changelog controller

---
+++ app/controllers/changelog_controller.rb
@@ -0,0 +1,2 @@
+class ChangelogController < ApplicationController
+end
---
+++ test/controllers/changelog_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ChangelogControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
