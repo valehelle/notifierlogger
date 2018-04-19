9b3d2aafa58cf66ba0568749c652c20ff13ed21a
Valehelle <hazmiirfan92@gmail.com>
2018-04-19 14:20:33 +0800

    enable create project

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,2 +1,26 @@
 class ProjectController < ApplicationController
+    before_action :authenticate_user!
+    def index
+        @projects = current_user.projects
+    end
+    def new
+    
+    end
+    
+    def show
+        @project = Project.find(params[:id])
+    end
+    
+    def create
+        @project = current_user.projects.new(project_params)
+        @project.webhook = 'sdlfjksdaf'
+        @project.secret = 'sdfsdfdsf'
+        @project.save
+        redirect_to @project
+    end
+
+    private
+    def project_params
+        params.require(:project).permit(:name)
+    end
 end
